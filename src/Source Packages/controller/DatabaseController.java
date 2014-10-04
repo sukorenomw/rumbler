637d347eb8507beadb8e441847e213f5283f00d7
vayneblue <vayneblue@yahoo.com>
2014-10-04 19:56:22 +0700

    benerin database control (select operation)

--- src/Source Packages/controller/DatabaseController.java
+++ src/Source Packages/controller/DatabaseController.java
@@ -5,10 +5,61 @@
  */
 package controller;
 
+import java.util.*;
+import model.Users;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  *
  * @author Epen
  */
 public class DatabaseController {
-    
+
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        DatabaseController dbc = new DatabaseController();
+        dbc.selectOperator(factory.openSession());
+    }
+
+    public List selectOperator(Session session) {
+        Transaction tx = null;
+        List results = null;
+        try {
+            tx = session.beginTransaction();
+            String sql = "SELECT * FROM Users";
+            SQLQuery query = session.createSQLQuery(sql);
+            query.addEntity(Users.class);
+            results = query.list();
+            for (Iterator itr = results.iterator(); itr.hasNext();) {
+                Users usr = (Users) itr.next();
+                System.out.println("user id = "+usr.getUserId() + "\nname : " + usr.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+    public void inputOperation(){
+        
+    }
+    public void deleteOperation(){
+        
+    }
+    public void updateOperation(){
+        
+    }
 }
